6cff673fd42505b70efaaa49eae0e0745e253248
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-15 10:29:41 +0100

    Update EnumUnitCaseTest.php

--- tests/EnumUnitCaseTest.php
+++ tests/EnumUnitCaseTest.php
@@ -6,20 +6,20 @@ use PHPUnit\Framework\TestCase;
 
 class EnumUnitCaseTest extends TestCase	
 {
-	public function testEnumUnitTestFound(): void
+    public function testEnumUnitTestFound(): void
     {
-		$searchTerm = 'TestEnumCase3';
+	$searchTerm = 'TestEnumCase3';
 		
-		$result = Controller::find($searchTerm);
+	$result = Controller::find($searchTerm);
 		
         $this->assertContains('Data type: Enum case value',$result);
     }
 	
-	public function testEnumUnitTestNotFound(): void
+    public function testEnumUnitTestNotFound(): void
     {
-		$searchTerm = 'wrongTestEnumCase';
+	$searchTerm = 'wrongTestEnumCase';
 		
-		$result = Controller::find($searchTerm);
+	$result = Controller::find($searchTerm);
 		
         $this->assertNotContains('Data type: Enum case value',$result); 
     } 
