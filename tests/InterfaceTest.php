b1c537eefe85f533cb41baa4d73bb7b124d79cc1
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-15 10:30:38 +0100

    Update InterfaceTest.php

--- tests/InterfaceTest.php
+++ tests/InterfaceTest.php
@@ -6,20 +6,20 @@ use PHPUnit\Framework\TestCase;
 
 class InterfaceTest extends TestCase	
 {
-	public function testInterfaceFound(): void
+    public function testInterfaceFound(): void
     {
-		$searchTerm = 'TestInterface';
+	$searchTerm = 'TestInterface';
 		
-		$result = Controller::find($searchTerm);
+	$result = Controller::find($searchTerm);
 		
         $this->assertContains('Data type: Interface',$result);
     }
 	
-	public function testInterfaceNotFound(): void
+    public function testInterfaceNotFound(): void
     {
-		$searchTerm = 'WrongTestInterface';
+	$searchTerm = 'WrongTestInterface';
 		
-		$result = Controller::find($searchTerm);
+	$result = Controller::find($searchTerm);
 		
         $this->assertNotContains('Data type: Interface',$result); 
     }	
