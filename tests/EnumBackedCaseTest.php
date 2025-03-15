ddfb8339ef3ab395ce151ad27005dc4b534e1d63
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-15 10:28:43 +0100

    Update EnumBackedCaseTest.php

--- tests/EnumBackedCaseTest.php
+++ tests/EnumBackedCaseTest.php
@@ -6,21 +6,21 @@ use PHPUnit\Framework\TestCase;
 
 class EnumBackedCaseTest extends TestCase	
 {
-	public function testBackedCaseTestFound(): void
+    public function testBackedCaseTestFound(): void
     {
-		$searchTerm = 'TestEnumBackedCase2 value';
+      $searchTerm = 'TestEnumBackedCase2 value';
 		
-		$result = Controller::find($searchTerm);
+      $result = Controller::find($searchTerm);
 		
-        $this->assertContains('Data type: Enum string case backing value',$result);
+      $this->assertContains('Data type: Enum string case backing value',$result);
     }
 	
-	public function testBackedCaseTestNotFound(): void
+    public function testBackedCaseTestNotFound(): void
     {
-		$searchTerm = 'wrong enum backed case value';
+         $searchTerm = 'wrong enum backed case value';
 		
-		$result = Controller::find($searchTerm);
+	 $result = Controller::find($searchTerm);
 		
-        $this->assertNotContains('Data type: Enum string case backing value',$result); 
+         $this->assertNotContains('Data type: Enum string case backing value',$result); 
     } 
 }
