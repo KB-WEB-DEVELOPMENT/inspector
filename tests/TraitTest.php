66cc4b226b7313eab2a355ded7b779eb600c725e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-15 08:13:21 +0100

    Create TraitTest.php

---
+++ tests/TraitTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Inspector\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class TraitTest extends TestCase	
+{
+	public function testTraitFound(): void
+    {
+		$searchTerm = 'TestOriginalTrait';
+		
+		$result = Controller::find($searchTerm);
+		
+        $this->assertContains('Data type: Trait',$result);
+    }
+	
+	public function testTraitNotFound(): void
+    {
+		$searchTerm = 'WrongTestOriginalTrait';
+		
+		$result = Controller::find($searchTerm);
+		
+        $this->assertNotContains('Data type: Trait',$result); 
+    }     
+}
