22b364044b542846be1bfcaa46e11465ae5f3363
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-15 08:07:40 +0100

    Update bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-if ( is_readable( __DIR__ . '/vendor/autoload.php') ) {
-    require_once __DIR__ . '/vendor/autoload.php';
+if ( is_readable( __DIR__ . '../vendor/autoload.php') ) {
+    require_once __DIR__ . '../vendor/autoload.php';
 }
