1ca6b0bc3b19ea8b77222653f7d6c63c954b61bf
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-11-30 14:07:04 +0100

    Update TraitTest.php

--- tests/TraitTest.php
+++ tests/TraitTest.php
@@ -8,18 +8,18 @@ class TraitTest extends TestCase
 {
     public function testTraitFound(): void
     {
-	$searchTerm = 'TestOriginalTrait';
+		$searchTerm = 'TestOriginalTrait';
 		
-	$result = Controller::find($searchTerm);
+		$result = Controller::find($searchTerm);
 		
         $this->assertContains('Data type: Trait',$result);
     }
 	
     public function testTraitNotFound(): void
     {
-	$searchTerm = 'WrongTestOriginalTrait';
+		$searchTerm = 'WrongTestOriginalTrait';
 		
-	$result = Controller::find($searchTerm);
+		$result = Controller::find($searchTerm);
 		
         $this->assertNotContains('Data type: Trait',$result); 
     }     
